92c98e731deb2e2467e485ed05db0af37b29af9c
Yibo Wang <yibo_wang@myyahoo.com>
2025-11-22 22:19:03 -0500

    add mps support

--- main.py
+++ main.py
@@ -6,7 +6,12 @@ from trainer import Trainer
 from utils import check_data_structure, evaluate_model
 
 # Set device
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+elif torch.backends.mps.is_available():
+    device = torch.device('mps')
+else:
+    device = torch.device('cpu')
 print(f"Using device: {device}")
 
 # ==================== Main Program ====================
